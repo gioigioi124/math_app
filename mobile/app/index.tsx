5d605ed288e188c70731d1735c247ee14c7223a5
gioigioi124 <gioigioi12.4@gmail.com>
2026-01-17 10:14:47 +0700

    reset về form tối giản

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,7 @@
+import "../global.css";
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
